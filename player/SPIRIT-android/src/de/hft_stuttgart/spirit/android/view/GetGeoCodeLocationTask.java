722271e3dfbc4227283e0fb10fe3d6f79c6a6b9d
Lukas <31ablu1bif@hft-stuttgart.de>
2015-05-04 14:31:49 +0200

    Fixed IndexoutOfBoundsException in GetGeoCodeLocationTask

--- player/SPIRIT-android/src/de/hft_stuttgart/spirit/android/view/GetGeoCodeLocationTask.java
+++ player/SPIRIT-android/src/de/hft_stuttgart/spirit/android/view/GetGeoCodeLocationTask.java
@@ -12,35 +12,42 @@ import de.hft_stuttgart.spirit.android.Story;
 
 public class GetGeoCodeLocationTask extends AsyncTask<Story, Void, String> {
 
-    	TextView view;
-    	Context context;
-    	
-    	public GetGeoCodeLocationTask(TextView view, Context context) {
-			this.view = view;
-			this.context = context;
-		}
-    	
-    	
-		@Override
-		protected String doInBackground(Story... params) {
-            Geocoder gc = new Geocoder(context);
-            try {
-				List<Address> address = gc.getFromLocation(params[0].getLatitude(), params[0].getLongitude(), 1);
+	TextView view;
+	Context context;
+
+	public GetGeoCodeLocationTask(TextView view, Context context) {
+		this.view = view;
+		this.context = context;
+	}
+
+	@Override
+	protected String doInBackground(Story... params) {
+		Geocoder gc = new Geocoder(context);
+		try {
+			List<Address> address = gc.getFromLocation(params[0].getLatitude(),
+					params[0].getLongitude(), 1);
+			if (address.isEmpty()) {
+				return "location not found";
+			} else {
 				String city = address.get(0).getLocality();
-				if(city == null) city = address.get(0).getAdminArea();
-				if(city == null) city = address.get(0).getCountryName();
-				if(city == null) return "Where is this !!!!";
+				if (city == null)
+					city = address.get(0).getAdminArea();
+				if (city == null)
+					city = address.get(0).getCountryName();
+				if (city == null)
+					return "Where is this !!!!";
 				return city;
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
-			return null;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		
-		@Override
-		protected void onPostExecute(String result) {
-			view.setText(result);
-		}
-    	
-    }
+		return null;
+	}
+
+	@Override
+	protected void onPostExecute(String result) {
+		view.setText(result);
+	}
+
+}
