8acee134ae0650b3866c87fdf096857906c88ce0
huth1 <22huth1bif@hft-stuttgart.de>
2015-06-21 20:21:04 +0200

    Update StoryFilter.java
    change url for StoryFilter

--- player/SPIRIT-android/src/de/hft_stuttgart/spirit/android/view/StoryFilter.java
+++ player/SPIRIT-android/src/de/hft_stuttgart/spirit/android/view/StoryFilter.java
@@ -277,8 +277,8 @@ public class StoryFilter implements Parcelable {
 		//Example: http://api.storytellar.de/story?author=arno+claus&size_max=20
 		//String URL = "http://api.storytellar.de/story?";
 		StringBuilder getQuery = new StringBuilder();
-		//getQuery.append("http://api.storytellar.de/story?"); 
-		getQuery.append("http://api.storytellar.de/temp?");
+		getQuery.append("http://api.storytellar.de/story?"); 
+		//getQuery.append("http://api.storytellar.de/temp?");
 		
 		if (this.title.trim().length() > 0){
 			String title = this.title;
