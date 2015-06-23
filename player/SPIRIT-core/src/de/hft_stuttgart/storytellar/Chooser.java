56672d56f4d3308c782e37f5a5c91d66f34d8832
Lukas <31ablu1bif@hft-stuttgart.de>
2015-06-23 14:31:09 +0200

    updated Chooser class

--- player/SPIRIT-core/src/de/hft_stuttgart/storytellar/Chooser.java
+++ player/SPIRIT-core/src/de/hft_stuttgart/storytellar/Chooser.java
@@ -17,19 +17,38 @@ public class Chooser implements Interaction, Serializable{
 	
 	private String question;
 	private List<String> answers;
-	private List<String> items; // Not available for normal quiz
+	private List<String> items;
+	private List<String> nextScenes;
 
 	public Chooser(){
 		this.answers = new ArrayList<String>();
 		this.items = new ArrayList<String>();
+		this.nextScenes = new ArrayList<String>();
 	}
 	
 	public Chooser(String question, List<String> answers, List<String> nextScenes, List<String> items){
 		this.question = question;
 		this.answers = answers;
+		this.nextScenes = nextScenes;
 		this.items = items;
 	}
 	
+	/**
+	 * 
+	 * @return the scenes, as list
+	 */
+	public List<String> getNextScenes() {
+		return nextScenes;
+	}
+
+	/**
+	 * 
+	 * @param nextScenes the scenes to set
+	 */
+	public void setNextScenes(List<String> nextScenes) {
+		this.nextScenes = nextScenes;
+	}
+
 	/**
 	 * @return the question, as String
 	 */
